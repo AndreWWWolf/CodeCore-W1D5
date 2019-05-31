6fd808d8b63b93737e1fd20db36bf701c171d4f1
AndreWWWolf <andrew.wolfeus@gmail.com>
2019-05-31 14:42:24 -0700

    objects merged successfully

--- mergeObject.js
+++ mergeObject.js
@@ -1,8 +1,14 @@
+// MERGING OBJECTS
+
+// USING OBJECT.ASSIGN (EASIEST WAY):
+
 // let objA = {a: 1, b: 2}
 // let objB = {c: 3, d:4}
 // let objC = Object.assign(objA, objB);
 
 // console.log(objC);
+
+// WITHOUT OBJECT.ASSIGN:
 
 function merge(objA, objB) {
     const mergeObj = {};
@@ -23,4 +29,9 @@ function merge(objA, objB) {
       sname: 'Shannon',
       smessage: 'Sand is brown'
   }
+//   if objA and objB have matching key: value variable names, as below, objB will overwrite objA when console.logged:
+//   const objB = {
+    // name: 'Shannon',
+    // message: 'Sand is brown'
+}
   console.log(merge(objA, objB));
